965a04d60e0314c1c1ae2ac1ede50e810af7b0c6
samuel wa kwatekwa <skamunua@gmail.com>
2023-11-27 13:05:17 +0300

    URA

--- models.js
+++ models.js
@@ -1,1 +1,26 @@
-//For models
+const mongoose = require('mongoose');
+
+const taxpayerSchema = new mongoose.Schema({
+    name: String,
+    dob: Date,
+    occupation: String,
+    gender: String,
+    phone: String,
+    email: String,
+    income: Number,
+    
+});
+
+const TaxPayer = mongoose.model('TaxPayer', taxpayerSchema);
+
+const assetSchema = new mongoose.Schema({
+    assetName: String,
+    estimatedCost: Number,
+    tin: String,
+    type: String,
+    
+});
+
+const Asset = mongoose.model('Asset', assetSchema);
+
+module.exports = { TaxPayer, Asset };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const path = require('path');
+
+const { TaxPayer, Asset } = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,30 +17,74 @@ mongoose.connect('mongodb+srv://kamunwa:30090Sam@cluster0.xm2mttk.mongodb.net/UR
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error: '))
 db.once('open', function () {
-    console.log('Connected successfully  to  Database  URA!');
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/', (req, res) => {
-    const name = req.body.name;
-    const dob = req.body.dob;
-    const occupation = req.body.occupation;
-    const gender = req.body.gender;
-    const phone = req.body.phone;
-    const email = req.body.email;
-    const income = req.body.income;
-
-    const year = new Date().getFullYear();
-    const genderLetter = gender.charAt(0).toUpperCase();
-    const uniqueNumber = Math.floor(100000 + Math.random() * 900000);
-
-    const taxId = `${year}/${genderLetter}/${uniqueNumber}`;
-
-    res.send(`Hello ${name}, your Tax Identification Number is ${taxId}`);
+    console.log('Connected successfully to Database URA!');
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+
+app.post('/create-taxpayer', async (req, res) => {
+    try {
+        const { name, dob, occupation, gender, phone, email, income } = req.body;
+
+        const newTaxPayer = new TaxPayer({
+            name,
+            dob,
+            occupation,
+            gender,
+            phone,
+            email,
+            income
+        });
+
+        await newTaxPayer.save();
+
+        res.status(201).json({ message: 'Taxpayer created successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+app.post('/create-asset', async (req, res) => {
+    try {
+        const { assetName, estimatedCost, tin, type } = req.body;
+
+        const newAsset = new Asset({
+            assetName,
+            estimatedCost,
+            tin,
+            type
+        });
+
+        await newAsset.save();
+
+        res.status(201).json({ message: 'Asset created successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+app.get('/get-taxpayers', async (req, res) => {
+    try {
+        const taxpayers = await TaxPayer.find({});
+        res.status(200).json({ taxpayers });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+app.get('/get-assets', async (req, res) => {
+    try {
+        const assets = await Asset.find({});
+        res.status(200).json({ assets });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
